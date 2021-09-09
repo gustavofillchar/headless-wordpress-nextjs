151941c212afcda5772fdab9ebb1b22660a0e611
Gustavo Lima <gustavo.fillchar@gmail.com>
2021-09-09 14:39:55 -0300

    revalidate teste

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -65,6 +65,7 @@ export async function getStaticProps({ params, preview = false, previewData }) {
   const data = await getPostAndMorePosts(params.slug, preview, previewData)
 
   return {
+    revalidate: 1,
     props: {
       preview,
       post: data.post,
